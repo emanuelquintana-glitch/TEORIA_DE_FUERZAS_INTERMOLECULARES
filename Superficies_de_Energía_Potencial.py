83c485d6ab977a54b7c7a1bfa92ac5416bc3ca63
Emanuel Quintana Silva <emanuel.quintana@uptc.edu.co>
2026-07-09 23:24:22 -0500

    Fix function signature typo in Superficies_de_Energía_Potencial.py

--- Superficies_de_Energía_Potencial.py
+++ Superficies_de_Energía_Potencial.py
@@ -4,7 +4,7 @@ Cálculos Ab Initio Rigurosos de Superficies de Energía Potencial
 SAPT (Symmetry-Adapted Perturbation Theory.) de Alta Precisión.
 """
 
-int man (void
+int man (voi
 
 # Importamos las libreias necesarias
 import numpy as np
